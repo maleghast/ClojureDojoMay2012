70bf07f76a6f7c1274c6575fe907aac8ca5a371c
Oliver Godby <oliver_godby@me.com>
2012-05-29 19:39:47 +0100

    First Commit
    Dojo for May
    
    Signed-off-by: Oliver Godby <oliver_godby@me.com>

---
+++ src/eventsourcer/core.clj
@@ -0,0 +1,1 @@
+(ns eventsourcer.core)
---
+++ test/eventsourcer/test/core.clj
@@ -0,0 +1,6 @@
+(ns eventsourcer.test.core
+  (:use [eventsourcer.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
