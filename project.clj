717a6de481d89ab06b0782a5e11f7d2b1369fb13
Mathieu Gauthron <mathieu@matlux.net>
2012-05-29 20:01:31 +0100

    added eclipse

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject eventsourcer "1.0.0-SNAPSHOT"
   :description "Event Sourced Data Mining Exercise for London Clojure Dojo"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dev-dependencies [[lein-eclipse "1.0.0"]])
