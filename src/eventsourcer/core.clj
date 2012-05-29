f2c529a17d8a33b234f671144a1547d711064d68
Oliver Godby <oliver_godby@me.com>
2012-05-29 20:07:12 +0100

    First try...
    Trying to read a logfile
    
    Signed-off-by: Oliver Godby <oliver_godby@me.com>

--- src/eventsourcer/core.clj
+++ src/eventsourcer/core.clj
@@ -1,1 +1,9 @@
 (ns eventsourcer.core)
+
+(use '[clojure.java.io] :only (reader))
+
+(defn taillog [file numlines]
+	(with-open [rdr (reader file)]
+		(take numlines (line-seq rdr))))
+
+(println (taillog "/var/log/kernel.log" 50))
