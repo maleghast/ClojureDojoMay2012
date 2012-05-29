73f5dc24309e3997a07a5dd8ef6d59e9821cec0e
Mathieu Gauthron <mathieu@matlux.net>
2012-05-29 20:46:28 +0100

    pprint added

--- src/eventsourcer/core.clj
+++ src/eventsourcer/core.clj
@@ -3,9 +3,11 @@
 (use '[clojure.java.io :only (reader)])
 
 (use '[clojure.string :only (join split)])
+(use '[clojure.pprint])
 
 (defn taillog [file numlines]
 	(with-open [rdr (reader file)]
 		(println (->> (line-seq rdr) (map #(clojure.string/split % #" "))))))
 
-(println (taillog "/var/log/kernel.log" 50))
+;(println (taillog "/var/log/kernel.log" 50))
+(pprint (taillog "/etc/passwd" 50))
