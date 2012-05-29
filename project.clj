3bf8e7b3848ed150fa13d72e40df6737f6047d60
Oliver Godby <oliver_godby@me.com>
2012-05-29 20:12:01 +0100

    Added main for namespace
    Needed to add this to allow lein run
    
    Signed-off-by: Oliver Godby <oliver_godby@me.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject eventsourcer "1.0.0-SNAPSHOT"
   :description "Event Sourced Data Mining Exercise for London Clojure Dojo"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[lein-eclipse "1.0.0"]])
+  :dev-dependencies [[lein-eclipse "1.0.0"]]
+  :main eventsourcer.core)
