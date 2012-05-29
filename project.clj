975b8e67de7854422ec24a8fe5b1f1f2cb225654
Mathieu Gauthron <mathieu@matlux.net>
2012-05-29 20:42:56 +0100

    split added

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject eventsourcer "1.0.0-SNAPSHOT"
   :description "Event Sourced Data Mining Exercise for London Clojure Dojo"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]]
   :main eventsourcer.core)
--- src/eventsourcer/core.clj
+++ src/eventsourcer/core.clj
@@ -2,8 +2,10 @@
 
 (use '[clojure.java.io :only (reader)])
 
+(use '[clojure.string :only (join split)])
+
 (defn taillog [file numlines]
 	(with-open [rdr (reader file)]
-		(take numlines (line-seq rdr))))
+		(println (->> (line-seq rdr) (map #(clojure.string/split % #" "))))))
 
 (println (taillog "/var/log/kernel.log" 50))
