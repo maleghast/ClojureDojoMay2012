5bcb3c57cc51f948cb9e559e7f36bea01ad7db8d
Chris Adams <wave@chrisadams.me.uk>
2012-05-29 19:45:45 +0100

    fix typo

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject eventsourcer "1.0.0-SNAPSHOT"
-  :description "Event Sourced Data Mining Exercise for London Clojre Dojo"
+  :description "Event Sourced Data Mining Exercise for London Clojure Dojo"
   :dependencies [[org.clojure/clojure "1.4.0"]])
